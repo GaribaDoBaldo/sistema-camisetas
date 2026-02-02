4ad2c8037e258013fe784979c9e460e0145154a7
GaribaDoBaldo <matheus.remf@gmail.com>
2026-02-02 11:59:56 -0300

    server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'segredo-local',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.send('Sistema rodando 🚀');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Servidor rodando na porta', PORT);
+});
